50b23b995f60401a6ef5c523c6ba75bb8a0f060d
EasonTekReviews <easontekreviews2398@gmail.com>
2020-10-11 17:50:49 +0800

    First Commit

---
+++ CalcPlusPlus/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  CalcPlusPlus
+//
+//  Created by EasonTekReviews on 14/4/20.
+//  Copyright © 2020 EasonTekReviews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var numberOnScreen:Double = 0;
+    var previousOnScreen:Double = 0;
+    var performingMath = false
+    var operation = 0;
+    var negative = false
+    var ver = false
+    func allclear() {
+        label.text = ""
+        previousOnScreen = 0;
+        operation = 0;
+        numberOnScreen = 0;
+        negative = false
+        ver = false
+    }
+    
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func button(_ sender: UIButton) {
+        if performingMath == true {
+            if sender.tag != 23 {
+                label.text = String(sender.tag - 1)
+                numberOnScreen = Double(label.text!)!
+                performingMath = false
+            } else if sender.tag == 23 {
+                label.text = "3.141592653589"
+            }
+        } else if sender.tag == 17 {
+            label.text = String(label.text! + ".")
+        } else if sender.tag == 23 {
+        } else if sender.tag == 19 {
+            if negative == false{
+                label.text = String("-" + label.text!)
+                negative = true
+            }
+            
+        } else {
+            if ver == false {
+                label.text = label.text! + String(sender.tag - 1)
+                numberOnScreen = Double(label.text!)!
+            } else if ver == true {
+                allclear()
+                label.text = label.text! + String(sender.tag - 1)
+                numberOnScreen = Double(label.text!)!
+            }  else if sender.tag == 20 {
+                label.text = String(numberOnScreen * numberOnScreen)
+            }  else if sender.tag == 21 {
+                label.text = String(numberOnScreen * numberOnScreen * numberOnScreen)
+            }
+        }
+  
+    }
+    
+    
+    @IBAction func operations(_ sender: UIButton) {
+        
+        if label.text != "" && sender.tag != 11 && sender.tag != 16 {
+            
+            previousOnScreen = Double(label.text!)!
+            operation = sender.tag
+            performingMath = true
+        }
+        else if sender.tag == 16 {
+            if operation == 12 {
+                label.text = String(previousOnScreen / numberOnScreen)
+            }
+            else if operation == 13 {
+                label.text = String(previousOnScreen * numberOnScreen)
+            }
+            else if operation == 14 {
+                label.text = String(previousOnScreen - numberOnScreen)
+            }
+            else if operation == 15 {
+                label.text = String(previousOnScreen + numberOnScreen)
+            }
+            else if operation == 22 {
+                label.text = String(pow(Double(previousOnScreen),(Double(numberOnScreen))))
+            }
+            else if operation == 27 {
+                sqrt(Double(previousOnScreen))
+            }
+            else if operation == 28 {
+                cbrt(Double(previousOnScreen))
+            }
+        } else if sender.tag == 11 {
+            allclear()
+        }
+        }
+    }
---
+++ README.md
@@ -0,0 +1,15 @@
+# calculatorplusplus
+An Open-Source Calculator Project!
+
+THIS IS THE APP STORE CONNECT BETA CHANNEL SOURCE CODE
+
+
+This is a remake of Calc++, an old project of mine.
+I am bringing it back to life now!
+
+Build Number Scheme:
+Digits 4&5 - Build
+Digit 3 - Subversion (1 means subversion 2, 2 means subversion 3, etc.)
+Digits 1&2 - Version (10, 5, 21, etc.)
+
+00104 means Version 0 Subversion 2 Build 04 or v0.2.
