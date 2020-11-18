550bf5e43095e9342b0210ae36b1312b2b3ba959
EasonTekReviews <easontekreviews2398@gmail.com>
2020-11-18 11:40:55 +0800

    Add mac Port

--- CalcPlusPlus/ViewController.swift
+++ CalcPlusPlus/ViewController.swift
@@ -99,7 +99,7 @@ class ViewController: UIViewController {
                     label.text = String("Version 1.2rc1")
                     ver = true
                 } else if label.text == "533411731" {
-                    label.text = String("Build 1313")
+                    label.text = String("Build 1315")
                     ver = true
                 }
             }
